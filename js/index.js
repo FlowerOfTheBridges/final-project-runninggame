b15c2e7b2d347269c408eca6e92ff6f83979207b
FlowerOfTheBridges <gfiordeponti@gmail.com>
2020-09-13 23:12:04 +0200

    fix

--- js/index.js
+++ js/index.js
@@ -57,7 +57,7 @@ function init() {
 }
 
 function checkAssets(numberOfAssets, condition) {
-    if (assetsLoaded != numberOfAssets) {
+    if (assetsLoaded < numberOfAssets) {
         updateLoading(assetsLoaded, numberOfAssets);
     }
     else if (condition) {
